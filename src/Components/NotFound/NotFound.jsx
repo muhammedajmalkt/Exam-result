ff5a7d4932e0aba622bfdc0c9f0db101bbe2c37f
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-06-10 19:23:59 +0530

    not found

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <i className='flex justify-center text-gray-500 mt-5'> Page Not Found</i>
+    <i className='flex justify-center text-gray-500 mt-5'>Sorry, the page not found</i>
   )
 }
 
